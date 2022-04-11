58acc3bf01fc5627c069537f3a6341d5463af7f2
Kyle Stewart <kylestew@gmail.com>
2022-04-11 09:34:02 -0600

    Project setup

---
+++ SimpleMetalRenderer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleMetalRenderer
+//
+//  Created by Kyle Stewart on 4/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleMetalRenderer/SimpleMetalRendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMetalRendererApp.swift
+//  SimpleMetalRenderer
+//
+//  Created by Kyle Stewart on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMetalRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
